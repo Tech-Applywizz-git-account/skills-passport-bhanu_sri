dcbb48aad4e993c258ad120cd679d42f46de7f7e
vivekkk <techteam@Applywizz.com>
2025-10-01 05:14:06 +0530

    Nithya Payment link add

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -502,13 +502,13 @@ const SignupPage: React.FC = () => {
             email: form.email,
             phone: form.phone,
             country_code: form.countryCode,
-            promo_code: "Bobby",
+            promo_code: "Nithya",
           }
         ]);
 
       if (leadsError) throw leadsError;
       // setTimeout(() => setShowDialog(false), 3000);
-      window.location.href = "https://www.paypal.com/ncp/payment/CF9PDNMVQANT4";
+      window.location.href = "https://www.paypal.com/ncp/payment/2AUT5DRKTE4UG";
 
     } catch (err: any) {
       setError(err.message || "Something went wrong. Try again.");
